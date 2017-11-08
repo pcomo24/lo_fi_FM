30295487e7511b8890b2b229cc3e1c064060fcd0
pcomo24 <pcomo24@gmail.com>
2017-11-07 23:39:30 -0600

    dabbling in changing JSX as state

--- lo_fi_fm/src/StartButton.jsx
+++ lo_fi_fm/src/StartButton.jsx
@@ -1,32 +1,37 @@
 import React, { Component } from 'react';
 
+//how to do this properly??
+const button = <button type='button' onClick={(event) => this.showVideo(event)}>tape</button>;
+//can i setState as function??...yes, but this.showVideo doesnt work
+function returnButton() {
+  return <button type='button' onClick={(event) => this.showVideo(event)}>tape</button>;
+}
+
 class StartButton extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      target: ''
+      tapeButton: returnButton()
     }
   }
   showVideo(event) {
-    console.log(event.target);
+    console.log(event);
     console.log('tape button clicked!');
-    console.log(this.props.target);
-    //how to change jsx that is rendered??
     this.setState({
-      target: event.target
+      tapeButton: 'tape button clicked!'
     })
     return <iframe src='https://www.youtube.com/watch?v=8i22oGH16Pw' height="200" width="300">
     </iframe>
   }
   render()  {
-  return <div>
-    <button type='button' onClick={(event) => console.log('radio button clicked!')}>
-    radio
-    </button>
-    <button type='button' onClick={(event) => this.showVideo(event)}>
-    tape
-    </button>
-  </div>
+  return (
+    <div>
+      <button type='button' onClick={(event) => console.log('radio button clicked!')}>
+      radio
+      </button>
+      {this.state.tapeButton}
+    </div>
+  )
   }
 
 }
