c9b223d4d3f02466e89493a21669db548ea2545a
pcomo24 <pcomo24@gmail.com>
2017-11-08 23:15:08 -0600

    toggle component with isHidden implemented

--- lo_fi_fm/src/StartButton.jsx
+++ lo_fi_fm/src/StartButton.jsx
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
-
-//how to do this properly??
-const button = <button type='button' onClick={(event) => this.showVideo(event)}>tape</button>;
-//can i setState as function??...yes, but this.showVideo doesnt work
-function returnButton() {
-  return <button type='button' onClick={(event) => this.showVideo(event)}>tape</button>;
-}
+import Button from './Button.jsx';
 
 class StartButton extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tapeButton: returnButton()
+      tapeButton: '',
+      isHidden: true
     }
   }
   showVideo(event) {
     console.log(event);
     console.log('tape button clicked!');
     this.setState({
-      tapeButton: 'tape button clicked!'
+      tapeButton: 'tape button clicked!',
+      isHidden : !this.state.isHidden
     })
     return <iframe src='https://www.youtube.com/watch?v=8i22oGH16Pw' height="200" width="300">
     </iframe>
@@ -29,7 +25,8 @@ class StartButton extends Component {
       <button type='button' onClick={(event) => console.log('radio button clicked!')}>
       radio
       </button>
-      {this.state.tapeButton}
+      <button type='button' onClick={(event) => this.showVideo(event)}>tape</button>
+      {this.state.isHidden && <Button/>}
     </div>
   )
   }
