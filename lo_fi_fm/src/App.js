0abe7ebcfaf5961817273697466d311cf27bee93
pcomo24 <pcomo24@gmail.com>
2017-11-20 23:40:03 -0600

    cleaned up syntax, better logic pseudocoded, start button showing and hiding next set of buttons

--- lo_fi_fm/src/App.js
+++ lo_fi_fm/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import StartButton from './StartButton';
+import SourceButtons from './SourceButtons';
 import './App.css';
 
-//have radio button and tapes button
-//onClick of radio button shows live channels(3) playing now
-//onClick of tapes button shows channels(3)
+//on home page load show start button(power button)
+//on click of start button hide start button, render radio and tape button
+//onClick of radio button request live channels(3) playing now and render as options
+//onClick of tapes button request channels(3) and render as options
 //once live channel or tape is selected bring up play/pause, rw, ff, skip
 
 
@@ -26,6 +27,11 @@ class App extends Component {
       testProps: 'props are being passed'
     }
   }
+  loadButtons(event) {
+    this.setState({
+      buttonsIsHidden: !this.state.buttonsIsHidden,
+    })
+  }
   onStartButtonClick(event) {
     console.log('start button clicked!');
     console.log(event.target);
@@ -40,7 +46,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.buttonsIsHidden && <StartButton testProps={this.state.testProps}/>}
+        <button onClick={(event) => this.loadButtons(event)}>Start</button>
+        {this.state.buttonsIsHidden && <SourceButtons testProps={this.state.testProps}/>}
 
 
       </div>
--- lo_fi_fm/src/SourceButtons.jsx
+++ lo_fi_fm/src/SourceButtons.jsx
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
 import Button from './Button.jsx';
 
-var StartButton = (props) => {
-
-
-  /*showVideo(event) {
-    console.log(event);
-    console.log(this.props.buttonVar)
-    })
-    return <iframe src='https://www.youtube.com/watch?v=8i22oGH16Pw' height="200" width="300">
-    </iframe>
+export default class SourceButton extends Component {
+  constructor(props) {
+    super(props);
   }
-  render()  { */
+
+  render()  {
   return (
     <div>
       <button type='button' onClick={(event) => console.log('radio button clicked!')}>
@@ -19,11 +14,8 @@ var StartButton = (props) => {
       </button>
       <button type='button' onClick={(event) => this.showVideo(event)}>tape</button>
       {/*this.state.isHidden && <Button/>*/}
-      {props.testProps}
+      {this.props.testProps}
     </div>
   )
-
-
+  }
 }
-
-export default StartButton;
